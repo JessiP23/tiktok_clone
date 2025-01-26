6d4b1d0a0bdb1b197ee3b407d09a800ebe150917
JessiP23 <jessi316866@gmail.com>
2025-01-25 20:51:52 -0500

    functional and non functional

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
+# Functional Requirements
+# User Profile
 # following an account
-# like a post
+# like video
+# View video
+# upload video
+# Create video
+# live streaming
+
+
+
+# Non-Functional Requirements
+# Storage capacity
+# Performance
+# Code maintainability
+
+
+# neural networks to analyze video content and user interactions, improvingthe accuracy of the recommendations
+#
+
 for i in range(10):
     print('hello world')
     i=i+1
